063d9229bcf69529162529c7ccffda07d1a7a7ba
Matus Tejiscak <ziman@functor.sk>
2015-04-27 13:25:29 +0100

    Emit data constructors.

--- src/IRTS/CodegenPython.hs
+++ src/IRTS/CodegenPython.hs
@@ -92,9 +92,26 @@ pythonLauncher = vcat . map text $
     , "  " ++ mangle (sMN 0 "runMain") ++ "()"
     ]
 
+-- idris's showCG is inexhaustive
+showCG'' :: Name -> String
+showCG'' (UN n) = T.unpack n
+showCG'' (NS n s) = showSep "." (map T.unpack (reverse s)) ++ "." ++ showCG n
+showCG'' (MN _ u) | u == txt "underscore" = "_"
+showCG'' (MN i s) = "{" ++ T.unpack s ++ show i ++ "}"
+showCG'' (SN s) = showCG' s
+  where showCG' (WhereN i p c) = showCG p ++ ":" ++ showCG c ++ ":" ++ show i
+        showCG' (WithN i n) = "_" ++ showCG n ++ "_with_" ++ show i
+        showCG' (InstanceN cl inst) = '@':showCG cl ++ '$':showSep ":" (map T.unpack inst)
+        showCG' (MethodN m) = '!':showCG m
+        showCG' (ParentN p c) = showCG p ++ "#" ++ show c
+        showCG' (CaseN c) = showCG c ++ "_case"
+        showCG' (ElimN sn) = showCG sn ++ "_elim"
+        showCG' (InstanceCtorN n) = showCG n ++ "_ictor"
+showCG'' NErased = "_"
+
 mangle :: Name -> String
 mangle (MN i n) | T.unpack n == "e" = T.unpack n ++ show i
-mangle n = "idris_" ++ concatMap mangleChar (showCG n)
+mangle n = "idris_" ++ concatMap mangleChar (showCG'' n)
   where
     mangleChar x
         | isAlpha x || isDigit x = [x]
@@ -120,7 +137,11 @@ cgApp f args =
     $+$ rparen
 
 cgDef :: (Name, DDecl) -> Doc
-cgDef (n, DConstructor name' tag arity) = empty
+cgDef (n, DConstructor name' tag arity) = cgDef (n, DFun name' args body)
+  where
+    args = [sMN i "e" | i <- [0..arity-1]]
+    body = DC Nothing tag n (map (DV . Glob) args)
+
 cgDef (n, DFun name' args body) =
     comment
     $+$ header
